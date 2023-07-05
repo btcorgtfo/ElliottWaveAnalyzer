bc168ae10120e2e25b233e6fe51efe131b7eb761
Dave Herman <daherman@users.noreply.github.com>
2023-07-05 18:01:04 -0400

    Add docs and tests for Monowave
    Adding tests and documentation to MonoWave feature

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023 Nightingale Professional Services, LLC
+# 
+# This code is licensed under a commercial License.
+# See LICENSE file for full text.
+from setuptools import setup, find_packages
+
+
+setup(
+    name="Elliott Wave Analyzer",
+    version="0.1",
+    packages=find_packages(),
+)
