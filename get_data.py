ff11162ea6f7017094321738ee9e3caab5fc4fc3
Michal Wawer <m.wawer@hotmail.com>
2024-06-05 18:32:52 +0200

    Update to external lib, save images, fix bugs

--- get_data.py
+++ get_data.py
@@ -2,8 +2,8 @@ import yfinance as yf
 
 from models.helpers import convert_yf_data
 
-df = yf.download(tickers='BTC-USD',
+df = yf.download(tickers='AAPL',
                  interval="1d",
-                 start="2017-12-01")
+                 start="2022-12-01")
 
-convert_yf_data(df).to_csv(r'data\btc-usd_1d_2017.csv', sep=",", index=False)
+convert_yf_data(df).to_csv(r'data\aapl_1d_2020.csv', sep=",", index=False)
